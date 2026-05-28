70bd376925d089911141def895990a76b6225fd0
shivaagapu <shivaramakrishna.agapu@gmail.com>
2026-05-28 16:22:19 +0530

    created emp class

---
+++ Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String name;
+    private int id;
+    private int salary;
+
+    public Employee(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
+}
