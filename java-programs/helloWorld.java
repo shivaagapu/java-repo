563b584f02059c4831f71d7c9ecbb8736ecbda78
shivaagapu <shivaramakrishna.agapu@gmail.com>
2026-05-28 14:58:24 +0530

    code changes done

--- java-programs/helloWorld.java
+++ java-programs/helloWorld.java
@@ -1,6 +1,6 @@
 public class helloWorld {
 public static void main(String[] args) {
-String name = "World";
+String name = "Hello World";
 String reverse = "";
 for ( int i = name.length() -1; i>=0; i--) {
     reverse += name.charAt(i);
